0474565d813f81ddf75b8bf3229743bf6ce1f5dd
Sreejith N <nssreejith229@gmail.com>
2019-05-28 08:16:33 +0000

    Updated material-ui and added config display to dashboard
    Material-UI updated to 4.0.1
    Used box for title of paper in configuration section of dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,16 @@
-import React, {Component} from 'react'
+import React, {Component,Fragment} from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import {Paper, List, ListItem, ListItemText, Chip, Box} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
+import SettingsIcon from '@material-ui/icons/Settings'
 import {withStyles} from '@material-ui/core/styles';
-import LoginButton from './components/LoginButton'
+import LoginButton from './components/LoginButton';
+import {compose} from 'redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {connect} from 'react-redux';
 var css = {
     root: {
       flexGrow: 1,
@@ -31,23 +36,49 @@ class Dashboard extends Component{
         }
 
     render(){
-        const {props,state} = this
+        const {props,state} = this;
+        const {admin} = this.props;
         return(
-            <div className={props.classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                    <IconButton className={props.classes.menuButton} color="inherit" aria-label="Menu"  onClick={props.toggleSideMenu}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" className={props.classes.grow}>
-                        {state.pageTitle}
-                    </Typography>
-                    <LoginButton/>
-                    </Toolbar>
-                </AppBar>
-            </div>
+            <Fragment>
+                <div className={props.classes.root}>
+                    <AppBar position="static">
+                        <Toolbar>
+                        <IconButton className={props.classes.menuButton} color="inherit" aria-label="Menu"  onClick={props.toggleSideMenu}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={props.classes.grow}>
+                            {state.pageTitle}
+                        </Typography>
+                        <LoginButton/>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+                <div className={props.classes.content}>
+                    <Paper m={6}>
+                    <Box display="flex" pl={2} pt={2}>
+                        <SettingsIcon/><Typography variant="body1" color='textPrimary'>Configuration</Typography>
+                    </Box>
+                        <List>
+                            <ListItem>
+                                <ListItemText>Location: {admin?admin.appSettings.location:""}</ListItemText>
+                            </ListItem>
+                        </List>
+                    </Paper>
+                </div>
+            </Fragment>
         )
     }
 }
+const mapStateToProps = (state)=>{
+    return {
+        admin:state.firestore.data.admin
+    }
+};
 
-export default  withStyles(css)(Dashboard)
+export default compose(
+    firestoreConnect([
+        {collection:'admin'}
+    ]),
+    connect(mapStateToProps,null),
+    withStyles(css)
+)(Dashboard)
